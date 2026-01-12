78f10ebc8babb69bbde3bcd0f4fb3d9406f6dbde
jerryalex2018 <okothalexjerry@gmail.com>
2026-01-12 19:13:37 +0300

    news changes

--- rugby.js
+++ rugby.js
@@ -2,102 +2,113 @@ let rugbyPlayers=[
     {
         image:"https://plus.unsplash.com/premium_photo-1752502981932-16a2c0ae9da1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D",
         name:"Lamech Nyamosi",
-        Position:"prop"
+        position:"prop"
     },
     {
-        image:"",
+        image:"https://plus.unsplash.com/premium_photo-1709220885302-4a351853b1fe?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTN8fHJ1Z2J5JTIwcGxheWVyc3xlbnwwfHwwfHx8MA%3D%3D",
         name:"Jerry okoth",
         position:"hooker"
     },
     {
-        image:"",
+        image:"https://plus.unsplash.com/premium_photo-1709196381057-093bbaca388c?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODl8fHJ1Z2J5JTIwcGxheWVyc3xlbnwwfHwwfHx8MA%3D%3D",
         name:"Adrew Mugera",
         position:"hooker"
     },
     {
-        image:"",
-        name:"",
-        position:""
+        image:"https://plus.unsplash.com/premium_photo-1708201429214-8eb0c4c901de?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODd8fHJ1Z2J5JTIwcGxheWVyc3xlbnwwfHwwfHx8MA%3D%3D",
+        name:"Antony Kiko",
+        position:"Hooker"
     },
      {
-        image:"",
-        name:"",
-        position:""
+        image:"https://images.unsplash.com/photo-1480699132165-c222f12b6c66?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODV8fHJ1Z2J5JTIwcGxheWVyc3xlbnwwfHwwfHx8MA%3D%3D",
+        name:"Miickey Omwami",
+        position:"Prop"
     },
      {
-        image:"",
-        name:"",
-        position:""
+        image:"https://images.unsplash.com/photo-1613425295295-f93e8d20beb3?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODF8fHJ1Z2J5JTIwcGxheWVyc3xlbnwwfHwwfHx8MA%3D%3D",
+        name:"Xavier Kevogo",
+        position:"Prop"
     },
      {
-        image:"",
-        name:"",
-        position:""
+        image:"https://images.unsplash.com/photo-1652107945878-73afa1eba6ad?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzd8fHJ1Z2J5JTIwcGxheWVyc3xlbnwwfHwwfHx8MA%3D%3D",
+        name:"Bruno Kamba",
+        position:" number 8"
     },
      {
-        image:"",
-        name:"",
-        position:""
+        image:"https://plus.unsplash.com/premium_photo-1709072152534-937c00d49216?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTAxfHxydWdieSUyMHBsYXllcnN8ZW58MHx8MHx8fDA%3D",
+        name:"Manuell Kyalo",
+        position:"Scrumhalf"
     },
      {
-        image:"",
-        name:"",
-        position:""
+        image:"https://images.unsplash.com/photo-1595432973730-d07ba6b406c2?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjJ8fHJ1Z2J5JTIwcGxheWVyc3xlbnwwfHwwfHx8MA%3D%3D",
+        name:"Fadhili Lionel",
+        position:"Flyhalf"
     },
      {
-        image:"",
-        name:"",
-        position:""
+        image:"https://images.unsplash.com/photo-1748112201782-3cf763a9b06e?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzB8fHJ1Z2J5JTIwcGxheWVyc3xlbnwwfHwwfHx8MA%3D%3D",
+        name:"Alvin Ingosi",
+        position:"Center"
     },
      {
-        image:"",
-        name:"",
-        position:""
+        image:"https://images.unsplash.com/photo-1508057187925-17094f679b10?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzR8fHJ1Z2J5JTIwcGxheWVyc3xlbnwwfHwwfHx8MA%3D%3D",
+        name:"Dominic Nyongesa",
+        position:"Center"
     },
      {
-        image:"",
-        name:"",
-        position:""
+        image:"https://plus.unsplash.com/premium_photo-1709220885441-89f9a60e60ea?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njh8fHJ1Z2J5JTIwcGxheWVyc3xlbnwwfHwwfHx8MA%3D%3D",
+        name:"Nathaniel",
+        position:"Prop"
     },
      {
-        image:"",
-        name:"",
-        position:""
+        image:"https://images.unsplash.com/photo-1506506791797-5a34262d8caa?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjF8fHJ1Z2J5JTIwcGxheWVyc3xlbnwwfHwwfHx8MA%3D%3D",
+        name:"Simon Kioko",
+        position:"flanker"
     },
      {
-        image:"",
-        name:"",
-        position:""
+        image:"https://media.istockphoto.com/id/1796577414/photo/senior-concentrated-young-african-american-man-rugby-player-sitting-on-filed-over-dark-empty.webp?a=1&b=1&s=612x612&w=0&k=20&c=zF6ENEah_gbjtNyCu4aETtOy9Abo6dmvfPL98Y7e8Ek=",
+        name:"Gizler Musembi",
+        position:"Prop"
     },
      {
-        image:"",
-        name:"",
-        position:""
+        image:"https://plus.unsplash.com/premium_photo-1709220884244-db197799737a?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjB8fHJ1Z2J5JTIwcGxheWVyc3xlbnwwfHwwfHx8MA%3D%3D",
+        name:"Evans Oseko",
+        position:"Lock"
     },
      {
-        image:"",
-        name:"",
-        position:""
+        image:"https://media.istockphoto.com/id/2158268376/photo/shot-of-a-group-of-young-men-playing-a-game-of-rugby.webp?a=1&b=1&s=612x612&w=0&k=20&c=zA5T7FxZc9Epd-ahs4KAdvh-tdkHuCOOR2L9vyV77xc=",
+        name:"clein Onyancha",
+        position:"Lock"
     },
      {
-        image:"",
-        name:"",
-        position:""
+        image:"https://media.istockphoto.com/id/2195029549/photo/celebrating-success-on-rugby-field.webp?a=1&b=1&s=612x612&w=0&k=20&c=YOvgQpKCQLWStXMutXNeVo2Avy6Q3Uoliyh4A2pBinc=",
+        name:"collins ombaso",
+        position:"Flanker"
     }
 ]
-let containerEl = document.querySelector("#rugplayer")
+let containerEl = document.querySelector(".rugplayer")
 rugbyPlayers.forEach((player)=>{
     let cover = document.createElement("div");
     cover.classList.add("cover-container")
-containerEl.append(cover)
+
 
 
     let imgContainer = document.createElement("div")
     imgContainer.classList.add("imgContainer")
-    cover.append(imgContainer);
     imgContainer.innerHTML = `
     <img src="${player.image}" alt="player.name">
 
     `
+    let name = document.createElement("div")
+    name.classList.add("nameEl")
+    name.innerHTML = `<p class="name">Name:${player.name}</p>
+    
+    `
+    let position = document.createElement("div")
+    position.classList.add("positionEl")
+    position.innerHTML = `<p class="name">Position:${player.position}</p>
+    
+    `
+    cover.append(imgContainer,name,position);
 
+containerEl.append(cover)
 })
